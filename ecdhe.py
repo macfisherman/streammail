eba66567bf77a3414ab047b7c3cfb23c4c5b89f0
Jeff Macdonald <macfisherman@gmail.com>
2016-07-15 23:32:53 -0400

    program to generate stream addresses

---
+++ ecdhe.py
@@ -0,0 +1,341 @@
+#!/usr/bin/env python
+# from http://andrea.corbellini.name/2015/05/17/elliptic-curve-cryptography-a-gentle-introduction/
+# modified to add base58 of secret
+
+import collections
+import random
+from hashlib import sha256
+from binascii import unhexlify
+import hashlib
+
+def long_to_bytes (val, endianness='big'):
+    """
+    Use :ref:`string formatting` and :func:`~binascii.unhexlify` to
+    convert ``val``, a :func:`long`, to a byte :func:`str`.
+
+    :param long val: The value to pack
+
+    :param str endianness: The endianness of the result. ``'big'`` for
+      big-endian, ``'little'`` for little-endian.
+
+    If you want byte- and word-ordering to differ, you're on your own.
+
+    Using :ref:`string formatting` lets us use Python's C innards.
+    """
+
+    # one (1) hex digit per four (4) bits
+    width = val.bit_length()
+
+    # unhexlify wants an even multiple of eight (8) bits, but we don't
+    # want more digits than we need (hence the ternary-ish 'or')
+    width += 8 - ((width % 8) or 8)
+
+    # format width specifier: four (4) bits per hex digit
+    fmt = '%%0%dx' % (width // 4)
+
+    # prepend zero (0) to the width, to zero-pad the output
+    s = unhexlify(fmt % val)
+
+    if endianness == 'little':
+        # see http://stackoverflow.com/a/931095/309233
+        s = s[::-1]
+
+    return s
+
+
+'''Base58 encoding
+
+Implementations of Base58 and Base58Check endcodings that are compatible
+with the bitcoin network.
+'''
+
+# This module is based upon base58 snippets found scattered over many bitcoin
+# tools written in python. From what I gather the original source is from a
+# forum post by Gavin Andresen, so direct your praise to him.
+# This module adds shiny packaging and support for python3.
+
+# 58 character alphabet used
+alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+
+if bytes == str:  # python2
+    iseq = lambda s: map(ord, s)
+    bseq = lambda s: ''.join(map(chr, s))
+    buffer = lambda s: s
+else:  # python3
+    iseq = lambda s: s
+    bseq = bytes
+    buffer = lambda s: s.buffer
+
+
+def b58encode(v):
+    '''Encode a string using Base58'''
+
+    if not isinstance(v, bytes):
+        raise TypeError("a bytes-like object is required, not '%s'" %
+                        type(v).__name__)
+
+    origlen = len(v)
+    v = v.lstrip(b'\0')
+    newlen = len(v)
+
+    p, acc = 1, 0
+    for c in iseq(v[::-1]):
+        acc += p * c
+        p = p << 8
+
+    result = ''
+    while acc > 0:
+        acc, mod = divmod(acc, 58)
+        result += alphabet[mod]
+
+    return (result + alphabet[0] * (origlen - newlen))[::-1]
+
+
+def b58decode(v):
+    '''Decode a Base58 encoded string'''
+
+    if not isinstance(v, str):
+        v = v.decode('ascii')
+
+    origlen = len(v)
+    v = v.lstrip(alphabet[0])
+    newlen = len(v)
+
+    p, acc = 1, 0
+    for c in v[::-1]:
+        acc += p * alphabet.index(c)
+        p *= 58
+
+    result = []
+    while acc > 0:
+        acc, mod = divmod(acc, 256)
+        result.append(mod)
+
+    return (bseq(result) + b'\0' * (origlen - newlen))[::-1]
+
+
+def b58encode_check(v):
+    '''Encode a string using Base58 with a 4 character checksum'''
+
+    digest = sha256(sha256(v).digest()).digest()
+    return b58encode(v + digest[:4])
+
+
+def b58decode_check(v):
+    '''Decode and verify the checksum of a Base58 encoded string'''
+
+    result = b58decode(v)
+    result, check = result[:-4], result[-4:]
+    digest = sha256(sha256(result).digest()).digest()
+
+    return check == digest[:4]
+
+EllipticCurve = collections.namedtuple('EllipticCurve', 'name p a b g n h')
+
+curve = EllipticCurve(
+    'secp256k1',
+    # Field characteristic.
+    p=0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f,
+    # Curve coefficients.
+    a=0,
+    b=7,
+    # Base point.
+    g=(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+       0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8),
+    # Subgroup order.
+    n=0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141,
+    # Subgroup cofactor.
+    h=1,
+)
+
+
+# Modular arithmetic ##########################################################
+
+def inverse_mod(k, p):
+    """Returns the inverse of k modulo p.
+
+    This function returns the only integer x such that (x * k) % p == 1.
+
+    k must be non-zero and p must be a prime.
+    """
+    if k == 0:
+        raise ZeroDivisionError('division by zero')
+
+    if k < 0:
+        # k ** -1 = p - (-k) ** -1  (mod p)
+        return p - inverse_mod(-k, p)
+
+    # Extended Euclidean algorithm.
+    s, old_s = 0, 1
+    t, old_t = 1, 0
+    r, old_r = p, k
+
+    while r != 0:
+        quotient = old_r // r
+        old_r, r = r, old_r - quotient * r
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+
+    gcd, x, y = old_r, old_s, old_t
+
+    assert gcd == 1
+    assert (k * x) % p == 1
+
+    return x % p
+
+
+# Functions that work on curve points #########################################
+
+def is_on_curve(point):
+    """Returns True if the given point lies on the elliptic curve."""
+    if point is None:
+        # None represents the point at infinity.
+        return True
+
+    x, y = point
+
+    return (y * y - x * x * x - curve.a * x - curve.b) % curve.p == 0
+
+
+def point_neg(point):
+    """Returns -point."""
+    assert is_on_curve(point)
+
+    if point is None:
+        # -0 = 0
+        return None
+
+    x, y = point
+    result = (x, -y % curve.p)
+
+    assert is_on_curve(result)
+
+    return result
+
+
+def point_add(point1, point2):
+    """Returns the result of point1 + point2 according to the group law."""
+    assert is_on_curve(point1)
+    assert is_on_curve(point2)
+
+    if point1 is None:
+        # 0 + point2 = point2
+        return point2
+    if point2 is None:
+        # point1 + 0 = point1
+        return point1
+
+    x1, y1 = point1
+    x2, y2 = point2
+
+    if x1 == x2 and y1 != y2:
+        # point1 + (-point1) = 0
+        return None
+
+    if x1 == x2:
+        # This is the case point1 == point2.
+        m = (3 * x1 * x1 + curve.a) * inverse_mod(2 * y1, curve.p)
+    else:
+        # This is the case point1 != point2.
+        m = (y1 - y2) * inverse_mod(x1 - x2, curve.p)
+
+    x3 = m * m - x1 - x2
+    y3 = y1 + m * (x3 - x1)
+    result = (x3 % curve.p,
+              -y3 % curve.p)
+
+    assert is_on_curve(result)
+
+    return result
+
+
+def scalar_mult(k, point):
+    """Returns k * point computed using the double and point_add algorithm."""
+    assert is_on_curve(point)
+
+    if k % curve.n == 0 or point is None:
+        return None
+
+    if k < 0:
+        # k * point = -k * (-point)
+        return scalar_mult(-k, point_neg(point))
+
+    result = None
+    addend = point
+
+    while k:
+        if k & 1:
+            # Add.
+            result = point_add(result, addend)
+
+        # Double.
+        addend = point_add(addend, addend)
+
+        k >>= 1
+
+    assert is_on_curve(result)
+
+    return result
+
+
+# Keypair generation and ECDHE ################################################
+
+def make_keypair():
+    """Generates a random private-public key pair."""
+    private_key = random.randrange(1, curve.n)
+    public_key = scalar_mult(private_key, curve.g)
+
+    return private_key, public_key
+
+
+print('Curve:', curve.name)
+
+# Alice generates her own keypair.
+alice_private_key, alice_public_key = make_keypair()
+print("Alice's private key:", hex(alice_private_key))
+print("Alice's public key: (0x{:x}, 0x{:x})".format(*alice_public_key))
+
+# Bob generates his own key pair.
+bob_private_key, bob_public_key = make_keypair()
+print("Bob's private key:", hex(bob_private_key))
+print("Bob's public key: (0x{:x}, 0x{:x})".format(*bob_public_key))
+
+# Alice and Bob exchange their public keys and calculate the shared secret.
+s1 = scalar_mult(alice_private_key, bob_public_key)
+s2 = scalar_mult(bob_private_key, alice_public_key)
+assert s1 == s2
+
+print('Shared secret: (0x{:x}, 0x{:x})'.format(*s1))
+
+# now create base56Check form
+
+# create byte arrays out of longs
+b1 = long_to_bytes(s1[0])
+b2 = long_to_bytes(s1[1])
+
+# add 0x04 to indicate uncompressed key
+key = bytes([0x04]) + b1 + b2
+
+# sha256 on 0x04 + b1 + b2
+sha = hashlib.new('sha256')
+sha.update(key)
+sha_digest = sha.digest()
+
+# ripemd160 on previous result
+ripe = hashlib.new('ripemd160')
+ripe.update(sha_digest)
+ripe_digest = ripe.digest()
+
+# add our version byte
+# convert secret into base58Check format
+version = 62
+address = b58encode_check(bytes([version]) + ripe_digest);
+print('Stream Address is {}'.format(address))
+assert(b58decode_check(address) == True)
+
+# code below is sanity check of base58 algo
+a =bytes( [ 0x00, 0x01, 0x09, 0x66, 0x77, 0x60, 0x06, 0x95, 0x3D, 0x55, 0x67,
+      0x43, 0x9E, 0x5E, 0x39, 0xF8, 0x6A, 0x0D, 0x27, 0x3B, 0xEE ])
+r = b58encode_check(bytes(a))
+assert( r == '16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM')
