d27c4861622c9c5faada161872d642a8b1ea5e79
Jeff Macdonald <macfisherman@gmail.com>
2016-07-30 15:48:34 -0400

    refactored

--- stream_test.go
+++ stream_test.go
@@ -10,23 +10,36 @@ import (
 
 const address = "SFwExaKH1iu2iK9gW3W2dnRQZewcmGkv6q"
 
-func TestNewStream(t *testing.T) {
-	os.RemoveAll(address)
-	data := map[string]string{"address": address}
-	buffer := new(bytes.Buffer)
-	json.NewEncoder(buffer).Encode(data)
-	resp, err := http.Post("http://localhost:8080/stream", "application/json", buffer)
-	if err != nil {
-		t.Error("error in POSTING", err)
-	}
-	// decode response
-	var v map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&v); err != nil {
-		t.Error("error in json decoding body", err)
-	}
+type addressCriteria struct {
+	Address string
+	Field   string
+	Want    string
+}
 
-	if v["ok"] != "address registered" {
-		t.Error("Expected [address registered], got", v["ok"])
+func TestStreamAddresses(t *testing.T) {
+	criteria := []addressCriteria{
+		{"SFwExaKH1iu2iK9gW3W2dnRQZewcmGkv6q", "ok", "address registered"},
+		{"1FwExaKH1iu2iK9gW3W2dnRQZewcmGkv6q", "error", "address not a STREAM address"},
+		{"SFwExaKH1iuZiK9gW3W2dnRQZewcmGkv6q", "error", "address format is invalid"}}
+
+	os.RemoveAll("SFwExaKH1iu2iK9gW3W2dnRQZewcmGkv6q")
+	for _, c := range criteria {
+		data := map[string]string{"address": c.Address}
+		buffer := new(bytes.Buffer)
+		json.NewEncoder(buffer).Encode(data)
+		resp, err := http.Post("http://localhost:8080/stream", "application/json", buffer)
+		if err != nil {
+			t.Error("error in POSTING", err)
+		}
+		// decode response
+		var v map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&v); err != nil {
+			t.Error("error in json decoding body", err)
+		}
+
+		if v[c.Field] != c.Want {
+			t.Errorf("Expected [%s], got [%s]", c.Want, v[c.Field])
+		}
 	}
 }
 
@@ -45,6 +58,6 @@ func TestMissingFieldAddress(t *testing.T) {
 	}
 
 	if v["error"] != "missing needed field, address" {
-		t.Error("Expected [missing needed field, address], got", v["ok"])
+		t.Error("Expected [missing needed field, address], got", v["error"])
 	}
 }
