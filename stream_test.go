9083765b603259f6a1b625337ec084193347e696
Jeff Macdonald <macfisherman@gmail.com>
2016-07-30 14:53:25 -0400

    better API

---
+++ stream_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"os"
+	"testing"
+)
+
+const address = "SFwExaKH1iu2iK9gW3W2dnRQZewcmGkv6q"
+
+func TestNewStream(t *testing.T) {
+	os.RemoveAll(address)
+	data := map[string]string{"address": address}
+	buffer := new(bytes.Buffer)
+	json.NewEncoder(buffer).Encode(data)
+	resp, err := http.Post("http://localhost:8080/stream", "application/json", buffer)
+	if err != nil {
+		t.Error("error in POSTING", err)
+	}
+	// decode response
+	var v map[string]interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&v); err != nil {
+		t.Error("error in json decoding body", err)
+	}
+
+	if v["ok"] != "address registered" {
+		t.Error("Expected [address registered], got", v["ok"])
+	}
+}
