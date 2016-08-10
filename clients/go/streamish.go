463b94553f3bf8ffbef2f88f3a4c1c5eb6e87f66
Jeff Macdonald <macfisherman@gmail.com>
2016-08-10 03:59:50 +0000

    simple command based client

--- clients/go/streamish.go
+++ clients/go/streamish.go
@@ -3,18 +3,72 @@ package main
 import (
     "flag"
     "fmt"
+    "io/ioutil"
+    "os"
+    "strings"
     "github.com/macfisherman/streammail/streamclient"
 )
 
+var uri *string
+var address *string
+
 func main() {
-    uri := flag.String("uri", "http://localhost:8080", "uri of the stream server")
-    address := flag.String("address", "", "address to operate against")
+    uri = flag.String("uri", "http://localhost:8080/stream/v1", "uri of the stream server")
+    address = flag.String("address", "", "address to operate against")
     flag.Parse()
 
-    // connect to server and get index
     stream := streamclient.NewStream(*uri, *address)
-    _, err := stream.GetIndex()
-    if err != nil {
-        fmt.Println(err)
+    command := strings.ToLower(flag.Arg(0))
+    switch command {
+        case "help":
+            help()
+        case "list":
+            index(stream)
+        case "read":
+            read(stream, flag.Arg(1))
+        case "post":
+            post(stream)
+        default:
+            fmt.Printf("Unknown command %s: valid commands are help, list, read, post", command)
     }
+}
+
+func help() {
+    fmt.Println("coming soon...")
+}
+
+func index(s *streamclient.Stream) {
+    list, err := s.GetIndex()
+    if err != nil {
+        fmt.Print("error:", err)
+        return
+    }
+
+    if len(list)==0 {
+        fmt.Println("No messages")
+        return
+    }
+
+    for i, msg := range list {
+        fmt.Printf("%d: %s\n", i, msg)
+    }
+}
+
+func read(s *streamclient.Stream, id string) {
+    msg, err := s.GetMessage(id)
+    if err != nil {
+        fmt.Println("error in reading message:", err)
+        return
+    }
+
+    fmt.Printf("%s\n", msg)
+}
+
+func post(s *streamclient.Stream) {
+    msg, err := ioutil.ReadAll(os.Stdin)
+    if err != nil {
+        fmt.Println("error reading stdin:", err)
+    }
+
+    s.PostMessage(string(msg))
 }
